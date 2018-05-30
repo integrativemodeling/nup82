f9ce20a0e8143b533744909f9d70bf4e7f20df40
Ben Webb <ben@salilab.org>
2018-05-30 16:32:57 -0700

    Fix use of SAXS data for validation
     - One SAXS profile was not used in the final publication.
     - Map sequence numbers from other species to those for yeast.
     - Nup82.2 was fitted against SAXS, not Nup82.1.

--- template/saxs.py
+++ template/saxs.py
@@ -17,8 +17,10 @@ class SAXSFits(object):
     def add_from_csv(self, model):
         with open(os.path.join(saxs_dir, 'Table6_SAXS.csv'), 'rb') as fh:
             for row in csv.DictReader(fh):
+                # 24367 was not used in publication due to low concentration
                 if row['FoXS fit score (chi value)'] \
-                   and row['Protein'] == 'NUP82':
+                   and row['Protein'] == 'NUP82' \
+                   and row['Protein ID'] != '24367':
                     yield self._add_one(row, model)
 
     def _add_one(self, row, model):
@@ -31,7 +33,14 @@ class SAXSFits(object):
         dataset = ihm.dataset.SASDataset(location=l)
         m = self.seqrange_re.match(row['Sequence coverage'])
         seqrange = (int(m.group(1)), int(m.group(2)))
-        # todo: check if fit is for 1st or 2nd copy
-        self.po._add_foxs_restraint(model, 'Nup82.1', seqrange, dataset,
+        # map seqrange for other species to that for yeast (manually)
+        # 9-445 (Candida glabrata) -> 4-452 (yeast)
+        # 791-899 (S. pombe) -> 572-690 (yeast)
+        if seqrange == (9,445):
+            seqrange = (4,452)
+        elif seqrange == (791,899):
+            seqrange = (572,690)
+        # Note that fit is for the 2nd copy of Nup82
+        self.po._add_foxs_restraint(model, 'Nup82.2', seqrange, dataset,
                          row['Rg'], row['FoXS fit score (chi value)'], None)
         return dataset
