6d8306ce04feaf761de07ee31a6fed27bd587679
Ben Webb <ben@salilab.org>
2018-06-28 07:38:41 -0700

    Relax test
    The exact number of lines in the mmCIF output file
    will depend on the exact versions of IHM, IMP
    and PMI that we use, so just ensure a reasonably
    large file was created for now. Ultimately we will
    employ IHM itself (once its read support is complete)
    to read the file back in and check its contents.

--- test/test.py
+++ test/test.py
@@ -47,7 +47,7 @@ class Tests(unittest.TestCase):
         # Check size of output file
         with open("nup82.cif") as fh:
             wcl = len(fh.readlines())
-        self.assertEqual(wcl, 46083)
+        self.assertTrue(wcl >= 46080)
 
     def test_nsp1(self):
         """Test generation of comparative models for Nsp1"""
